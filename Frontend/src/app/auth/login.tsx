3ae8f8fd9c1ca1ac0662a423f7dede889dc02f90
MariaDelazari <m.delazari.dev@gmail.com>
2026-06-17 20:55:25 -0300

    feat: adiciona identidade visual com logo do bolao

--- Frontend/src/app/auth/login.tsx
+++ Frontend/src/app/auth/login.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { View, Text, ImageBackground } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  ImageBackground,
+} from "react-native";
 
 export default function Login() {
   return (
@@ -7,7 +12,21 @@ export default function Login() {
       source={require("../../../assets/images/fundo-login.jpg")}
       style={{ flex: 1 }}
     >
-      <View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          source={require("../../../assets/images/bola.png")}
+          style={{
+            width: 120,
+            height: 120,
+          }}
+        />
+
         <Text>Tela de Login</Text>
       </View>
     </ImageBackground>
