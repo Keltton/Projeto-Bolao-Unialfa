d8a74039977db7890a158710e06c226ce14f6991
MariaDelazari <m.delazari.dev@gmail.com>
2026-06-17 21:09:12 -0300

    feat: adiciona identidade visual na tela de recuperacao

--- Frontend/src/app/auth/recuperar-senha.tsx
+++ Frontend/src/app/auth/recuperar-senha.tsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { Image } from "react-native";
 import { View, Text } from "react-native";
 
 export default function ForgotPassword() {
   return (
     <View>
+        <Image
+        source={require("../../../assets/images/bola.png")}
+        style={{
+          width: 70,
+          height: 70,
+        }}
+      />
       <Text>Recuperar Senha</Text>
+
+      <Text>
+        Informe seu e-mail para receber as instruções de recuperação.
+      </Text>
+
     </View>
   );
 }
